2b27bd40660ea2d55fe108486406946bbdfa6689
robhorbury <roberthorbury@hotmail.com>
2024-05-10 11:12:26 +0100

    Added some more keymaps

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -22,8 +22,10 @@ vim.keymap.set('n', '<leader>e', function()
   harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
 
-vim.keymap.set('n', 'oo', 'o<Esc>')
-vim.keymap.set('n', 'OO', 'O<Esc>')
+vim.keymap.set('n', '<leader>o', 'o<Esc>')
+vim.keymap.set('n', '<leader>O', 'O<Esc>')
+vim.keymap.set('v', 'p', '"_dP')
+vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Esc' })
 
 vim.keymap.set('n', '<leader>ga', function()
   vim.cmd 'G add .'
@@ -43,3 +45,14 @@ vim.keymap.set('n', '<leader>pl', function()
   vim.cmd 'G fetch'
   vim.cmd 'G pull'
 end)
+
+vim.keymap.set('n', '≠', ':resize -2<CR>')
+vim.keymap.set('n', '‘', ':resize +2<CR>')
+vim.keymap.set('n', 'æ', ':vertical resize -2<CR>')
+vim.keymap.set('n', '«', ':vertical resize +2<CR>')
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
