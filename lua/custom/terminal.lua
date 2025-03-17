f7a2977ccece25865ac47217959075116d9125d5
robhorbury <roberthorbury@hotmail.com>
2025-03-17 16:00:40 +0000

    feat: Updated terminal config

--- lua/custom/terminal.lua
+++ lua/custom/terminal.lua
@@ -7,9 +7,16 @@ vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]])
 vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]])
 vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
 
-vim.keymap.set('n', '<leader>tk', '<cmd>ToggleTerm size=20 dir=git_dir direction=horizontal<cr>')
-vim.keymap.set('n', '<leader>tj', '<cmd>ToggleTerm size=90 dir=git_dir direction=vertical<cr>')
-vim.keymap.set('n', '<leader>th', '<cmd>ToggleTerm dir=git_dir direction=float<cr>')
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    vim.cmd 'ToggleTerm size=15 dir=git_dir direction=horizontal'
+    vim.cmd 'ToggleTermToggleAll'
+  end,
+})
+
+vim.keymap.set('n', '<leader>tk', '<cmd>ToggleTerm size=15 dir=git_dir direction=horizontal<cr>')
+vim.keymap.set('n', '<leader>tl', '<cmd>ToggleTerm size=70 dir=git_dir direction=vertical<cr>')
+vim.keymap.set('n', '<leader>tj', '<cmd>ToggleTerm dir=git_dir direction=float<cr>')
 vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTermToggleAll<cr>')
 vim.keymap.set('n', '<leader>ts', '<cmd>TermSelect<cr>')
 vim.keymap.set('n', '<leader>tn', '<cmd>TermNew<cr>')
