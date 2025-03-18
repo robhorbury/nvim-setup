17f6bbf786fd01eb5ac94a17ee8ee201bd0b5c1d
rob <robert.horbury@hotmail.com>
2025-03-18 13:00:45 +0000

    feat: Added googling

--- lua/custom/googling.lua
+++ lua/custom/googling.lua
@@ -12,10 +12,13 @@ vim.keymap.set('v', '<leader>gl', function()
   search = search:gsub('"', ''):gsub("'", '')
 
   -- Construct the Google search URL
-  local search_string = 'Open "https://google.com/search?q=' .. search .. ' &"'
-
-  -- Execute the command in a terminal
-  vim.api.nvim_command("TermExec cmd='" .. search_string .. "'")
+  if vim.fn.has 'win32' then
+    local search_string = 'start https://google.com/search?q=' .. search
+    vim.api.nvim_command('TermExec cmd="' .. search_string .. '"')
+  else
+    local search_string = 'Open "https://google.com/search?q=' .. search .. ' &"'
+    vim.api.nvim_command("TermExec cmd='" .. search_string .. "'")
+  end
 end)
 
 vim.keymap.set('x', '<leader>gl', function()
@@ -39,5 +42,10 @@ vim.keymap.set('x', '<leader>gl', function()
 end)
 
 vim.keymap.set('n', '<leader>gl', function()
-  vim.api.nvim_command 'TermExec cmd=\'Open "https://google.com/"\''
+  if vim.fn.has 'win32' then
+    vim.api.nvim_command 'TermExec cmd="start https://google.com/"'
+    vim.api.nvim_command 'ToggleTermToggleAll'
+  else
+    vim.api.nvim_command 'TermExec cmd=\'Open "https://google.com/"\''
+  end
 end)
