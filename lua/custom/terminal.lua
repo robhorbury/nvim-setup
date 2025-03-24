9c87cf547003ebe9bc806465ce93ca9350c4b9ba
robhorbury <roberthorbury@hotmail.com>
2025-03-24 21:27:07 +0000

    feat: dev setup

--- lua/custom/terminal.lua
+++ lua/custom/terminal.lua
@@ -1,4 +1,12 @@
-if vim.fn.has 'win32' then
+local uname = vim.loop.os_uname()
+
+_G.OS = uname.sysname
+_G.IS_MAC = OS == 'Darwin'
+_G.IS_LINUX = OS == 'Linux'
+_G.IS_WINDOWS = OS:find 'Windows' and true or false
+_G.IS_WSL = IS_LINUX and uname.release:find 'Microsoft' and true or false
+
+if _G.IS_WINDOWS then
   local bash_options = {
     shell = 'bash.exe',
     shellcmdflag = '--login -i -c',
