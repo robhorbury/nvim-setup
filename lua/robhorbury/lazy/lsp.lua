edef2269b47ce9762eed8f301ae0b750e3bd8382
Rob Horbury <roberthorbury@hotmail.com>
2025-04-14 16:21:13 +0100

    automated dev commit

--- lua/robhorbury/lazy/lsp.lua
+++ lua/robhorbury/lazy/lsp.lua
@@ -104,6 +104,9 @@ return {
               isort = {
                 enabled = false,
               },
+              pyflakes = {
+                enabled = false,
+              },
             },
           },
         },
