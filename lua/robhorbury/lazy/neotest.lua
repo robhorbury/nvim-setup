ff1f0221ba0b13104150319a01a457c4fec9216e
Rob Horbury <128512467+robhorbury@users.noreply.github.com>
2025-04-30 11:28:47 +0100

    neotest

---
+++ lua/robhorbury/lazy/neotest.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/nvim-nio",
+      "nvim-neotest/neotest-python",
+    },
+    config = function()
+      require("neotest").setup {
+        adapters = {
+          require "neotest-python" {
+            dap = { justMyCode = false }, -- optional: for debugging
+            runner = "pytest", -- use pytest
+          },
+        },
+      }
+
+      local map = vim.keymap.set
+      map("n", "<leader>tt", function()
+        require("neotest").run.run()
+      end, { desc = "Run nearest test" })
+      map("n", "<leader>tf", function()
+        require("neotest").run.run(vim.fn.expand "%")
+      end, { desc = "Run file tests" })
+      map("n", "<leader>ts", function()
+        require("neotest").summary.toggle()
+      end, { desc = "Toggle test summary" })
+      map("n", "<leader>to", function()
+        require("neotest").output.open { enter = true }
+      end, { desc = "Show test output" })
+    end,
+  },
+}
